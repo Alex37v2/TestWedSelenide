d2cd4c2a80067dbd23e058f9e85b06b92e8f7fc0
Алексей Александров <Alex37v2@gmail.com>
2023-02-01 02:44:56 +1000

    added combined selector

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -43,22 +43,22 @@ class CallbackTest {
     void shouldTestV1() {
         driver.get("http://localhost:9999");
         List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Кристина Шибаева-Шарикова");
+        elements.get(0).sendKeys("Кристина Шлейка-Шарикова");
         elements.get(1).sendKeys("+79147988888");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__content")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String text = driver.findElement(By.cssSelector("p[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
     void shouldTestV2() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Кристина Шибаева-Шарикова");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Кристина Шлейка-Шарикова");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79270000000");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.cssSelector(".button__text")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        driver.findElement(By.cssSelector("button[role='button'][type='button']")).click();
+        String text = driver.findElement(By.cssSelector("p[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
